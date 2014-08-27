65405cb1d4a6a2f68345b4b6c01927d0ab229814
Shaun Harker <sharker81@gmail.com>
2014-08-27 11:43:08 -0400

    Made Closure.h more generic

--- include/chomp/Closure.h
+++ include/chomp/Closure.h
@@ -7,35 +7,34 @@
 
 #include "chomp/Complex.h"
 #include "chomp/Chain.h"
-#include <vector>
-#include "boost/unordered_set.hpp"
 
 namespace chomp {
   
-  inline void closure
-  ( std::vector < boost::unordered_set < Index > > & indices,
-   const Complex & complex ) {
+  template < class VectorOfSets > void 
+  closure ( VectorOfSets & indices,
+            Complex const& complex ) {
     int D = complex . dimension ();
     indices . resize ( D + 1 );
     for ( int d = D; d > 0; -- d ) {
-      BOOST_FOREACH ( Index i, indices [ d ] ) {
+      for ( Index i : indices [ d ] ) {
         Chain bd = complex . boundary ( i, d );
-        BOOST_FOREACH ( const Term & t, bd () ) {
+        for ( const Term & t : bd () ) {
           indices [ d - 1 ] . insert ( t . index () );
         }
       }
     }
   }
   
-  inline void star
-  ( std::vector < boost::unordered_set < Index > > & indices,
-   const Complex & complex ) {
+  
+  template < class VectorOfSets > void 
+  star ( VectorOfSets & indices,
+         Complex const& complex ) {
     int D = complex . dimension ();
     indices . resize ( D + 1 );
     for ( int d = 0; d < D; ++ d ) {
-      BOOST_FOREACH ( Index i, indices [ d ] ) {
+      for ( Index i : indices [ d ] ) {
         Chain cbd = complex . coboundary ( i, d );
-        BOOST_FOREACH ( const Term & t, cbd () ) {
+        for ( const Term & t : cbd () ) {
           indices [ d + 1 ] . insert ( t . index () );
         }
       }
