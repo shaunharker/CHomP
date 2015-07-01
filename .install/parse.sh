30d5925af8e8de540793c227634c303744e0d2e4
Shaun Harker <sharker81@gmail.com>
2015-07-01 11:37:40 -0400

    debugging travis build

--- .install/parse.sh
+++ .install/parse.sh
@@ -2,6 +2,13 @@
 ## Parse command line arguments
 ## !! --> Also, will mkdir the install location if it does not exist!
 
+echo parse 
+echo $PREFIX
+echo $BUILDTYPE
+echo $SEARCHPATH
+echo $TEST
+echo $MASS
+echo "\"$@\""
 for i in "$@"; do case $i in
     -p=*|--prefix=*) PREFIX="${i#*=}";         shift;;
     -b=*|--build=*) BUILDTYPE="${i#*=}";       shift;;
@@ -11,7 +18,7 @@ for i in "$@"; do case $i in
 esac; done
 absolute() { echo "$(cd "$(dirname "$1")" && pwd)/$(basename "$1")"; }
 if [ ! -z "$PREFIX" ]; then 
-  mkdir -p $PREFIX || ( echo Permission denied && exit 1 ); 
+  mkdir -p $PREFIX || ( echo Permission to mkdir $PREFIX denied && exit 1 ); 
   PREFIX=$(absolute $PREFIX); 
 elif [ -w /usr/local ]; then 
   PREFIX=/usr/local; 
@@ -20,7 +27,15 @@ elif [ -d ~/.local ]; then
 else
   echo Run with admin privileges or choose a non-system install path with --prefix && exit 1; 
 fi;
-if [ ! -w $PREFIX ]; then echo Permission denied && exit 1; fi
+if [ ! -w $PREFIX ]; then echo Permission to write to $PREFIX denied && exit 1; fi
 if [ -z "$BUILDTYPE" ]; then BUILDTYPE=Release; fi
 if [ -z "$SEARCHPATH" ]; then SEARCHPATH=$PREFIX; fi
 if [ -d "$SEARCHPATH" ]; then SEARCHPATH=$(absolute $SEARCHPATH); else exit 1; fi
+
+echo parse 
+echo $PREFIX
+echo $BUILDTYPE
+echo $SEARCHPATH
+echo $TEST
+echo $MASS
+
